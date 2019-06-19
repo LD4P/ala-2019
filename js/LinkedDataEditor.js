b0022dc99d4c66246b480ef44252e1a676df4d7d
Jeremy Nelson <jermnelson@gmail.com>
2019-06-19 16:42:35 -0600

    Afternoon save point

--- js/LinkedDataEditor.js
+++ js/LinkedDataEditor.js
@@ -20,6 +20,12 @@ export default class LinkedDataEditor extends HTMLElement {
       p {
         padding: .5em;
       }
+      pre {
+        background-color: black;
+        font-weight: bold;
+        color: white; 
+        margin: .5em;
+      }
     </style>
     <div>
       <h1>Linked Data Editor</h1>
@@ -42,6 +48,90 @@ export default class LinkedDataEditor extends HTMLElement {
       <a href="https://sinopia.io/editor" alt="Sinopia's Editor Tab">
        <img src="img/sinopia-editor-work-title.png" alt="Sinopia Editor Tab with Work Title Resource Template">
       </a>
+      <p>
+       Adding text to the form:
+      </p>
+      <img src="img/sinopia-editor-work-title-populated.png" alt="Work Title Resource Template Populated " />
+      <p>
+       The state of the application is represented in a client-side javascript object that looks
+       like:
+      </p>
+      <pre>
+      resource: {
+        'resourceTemplate:bf2:WorkTitle': {
+          'http://id.loc.gov/ontologies/bibframe/mainTitle': {
+            items: [
+              {
+                content: 'Pride and Prejudice',
+                id: 'KDzMdJeHe',
+                lang: {
+                  items: [
+                    {
+                      id: 'en',
+                      label: 'English'
+                    }
+                  ]
+                }
+              }
+            ]
+          },
+          'http://id.loc.gov/ontologies/bibframe/partName': {
+            items: [
+              {
+                content: 'The next generation',
+                id: 'sjN0et6uB',
+                lang: {
+                  items: [
+                    {
+                      id: 'en',
+                      label: 'English'
+                    }
+                  ]
+                }
+              }
+            ]
+          },
+          'http://id.loc.gov/ontologies/bibframe/partNumber': {
+            items: [
+              {
+                content: 'Two',
+                id: '4HjSOjShb',
+                lang: {
+                  items: [
+                    {
+                      id: 'en',
+                      label: 'English'
+                    }
+                  ]
+                }
+              }
+            ]
+          },
+          'http://id.loc.gov/ontologies/bibframe/note': {
+            OOQO1jGCYR: {
+              'resourceTemplate:bf2:Title:Note': {
+                'http://www.w3.org/2000/01/rdf-schema#label': {
+                  items: [
+                    {
+                      content: 'A title note on Pride and Prejudice: The next generation',
+                      id: '49ogIcHSZ',
+                      lang: {
+                        items: [
+                          {
+                            id: 'en',
+                            label: 'English'
+                          }
+                        ]
+                      }
+                    }
+                  ]
+                }
+              }
+            }
+          }
+        }
+      }
+      </pre>
     </div>
     `
   }
--- js/SinopiaServer.js
+++ js/SinopiaServer.js
@@ -9,9 +9,30 @@ export default class SinopiaServer extends HTMLElement {
       h1 {
         text-align: center;
       }
+      img {
+        width: 800px;
+      }
     </style>
     <div>
-      <h1>Sinopia Server</h1>
+      <h1>Linked Data Editor &amp; Sinopia Server</h1>
+      <p>
+        The Linked Data Editor generates RDF based on the state of the application.
+        Clicking on the <strong>Preview RDF</strong> button will display the RDF
+        graph:
+      </p>
+      <img src="img/sinopia-preview-rdf-modal.png" alt="Preview RDF Modal with RDF graph" />
+      <p>
+        Clicking on the <img src="img/save-and-publish-button.png" style="width: 100px" alt="Save and Publish Button" />
+        brings up a modal to select a group to save the RDF graph to and then the
+        RDF is stored and managed in an instance of
+        <a href="https://www.trellisldp.org/">Trellis</a>, a Linked-Data Platform,
+       running on Amazon Web Services.
+     </p>
+     <p>
+       The Sinopia Server returns the Trellis URI which can then be seen if you click on the
+      <strong>Preview RDF</strong> button again:
+     </p>
+     <img src ="img/sinopia-preview-saved-rdf.png" alt="Preview RDF Modal with saved Trellis URI" />
     </div>
     `
   }
